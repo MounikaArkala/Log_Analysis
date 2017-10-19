9d07a6cd3584ad7f2e86abf0245a905b17e49da0
Mounika Arkala <arkalamounika2404@gmail.com>
2017-10-19 16:56:47 -0500

    Update loganalysis.py

--- loganalysis.py
+++ loganalysis.py
@@ -1,46 +1,45 @@
+#!/usr/bin/env python
 import psycopg2
 # tabulate module is used to print the contents in table format
 from tabulate import tabulate
 
-DBNAME = "news"
-# connection of database
-db = psycopg2.connect(database=DBNAME)
-c = db.cursor()
-    
+
+def get_query_results(query):
+    """This function connects to the database, executes the query and prints
+    the results"""
+    db = psycopg2.connect(database="news")
+    c = db.cursor()
+    c.execute(query)
+    result = c.fetchall()
+    print tabulate(result, headers=[c.description[0][0], c.description[1][0]])
+    print""
+    db.close()
+
 
 def popular_articles():
     """This function returns popular 3 articles of all the time"""
-    c.execute("""select * from popular_articles 
-                 order by access_count desc limit 3""")
-    posts = c.fetchall()
+    query = "select * from popular_articles order by access_count desc limit 3"
     print""
     print "******Printing the most popular articles of all the time******"
-    print_table(posts)
+    posts = get_query_results(query)
 
 
 def popular_authors():
     """This function returns authors list according to their popularity"""
-    c.execute("select * from popular_authors order by totalaccess_count desc")
-    posts = c.fetchall()
+    query = "select * from popular_authors order by totalaccess_count desc"
     print "******Printing the most popular article authors of all time******"
-    print_table(posts)
+    posts = get_query_results(query)
 
 
 def error_percent():
-    """This function returns the result of days with more than 
-    1 requests leading to errors"""
-    c.execute("select * from error_percent where percentage>1.0")
-    posts = c.fetchall()
+    """This function returns the result of days with more than 1 requests
+    leading to errors"""
+    query = "select * from error_percent where percentage>1.0"
     print"***printing days with more than 1% of requests leading to errors***"
-    print_table(posts)
+    posts = get_query_results(query)
 
 
-def print_table(table_name):
-    """prints the table contents"""
-    print tabulate(table_name, 
-                   headers=[c.description[0][0], c.description[1][0]])
-    print""
-    
-popular_articles()
-popular_authors()
-error_percent()   
+if __name__ == "__main__":
+    popular_articles()
+    popular_authors()
+    error_percent()
